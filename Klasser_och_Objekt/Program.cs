98c83ae749c019bac79b23714bc73ef4ad03c532
Jesper Johansson <jesper.johansson@iths.se>
2024-09-19 14:32:53 +0200

    Övning 7 halvt klar

--- Klasser_och_Objekt/Program.cs
+++ Klasser_och_Objekt/Program.cs
@@ -1,25 +1,48 @@
-﻿
-using System.Runtime.CompilerServices;
+﻿//*** NOTERINGAR **** ///
 
-Person person1 = new Person { firstName = "Greger", lastName = "Johansson" };
-Person person2 = new Person { firstName = "Lukas", lastName = "Börjesson" };
 
-Console.WriteLine($"Namnet på person 1 är: {person1.firstName} {person1.lastName}");
-Console.WriteLine($"Namnet på person 2 är: {person2.firstName} {person2.lastName}");
+//Person person1 = new Person { firstName = "Greger", lastName = "Johansson" };
+//Person person2 = new Person { firstName = "Lukas", lastName = "Börjesson" };
 
-Console.WriteLine($"Person1 namn baklänges är: {person1.GetFullNameReversed()}");
-Console.WriteLine($"Person2 namn med titel är: {person2.GetFullName("Mr.")}");
+//Console.WriteLine($"Namnet på person 1 är: {person1.firstName} {person1.lastName}");
+//Console.WriteLine($"Namnet på person 2 är: {person2.firstName} {person2.lastName}");
 
-// 5. Lägg till så personer kan ha föräldrar.
-//Uppdatera person-klassen så att varje person kan ha en mamma och en pappa.
-//Dessa representeras som publika fält(fields) av typen Person
-//(eller vad du kallade klassen i uppgift 1).
+//Console.WriteLine($"Person1 namn baklänges är: {person1.GetFullNameReversed()}");
+//Console.WriteLine($"Person2 namn med titel är: {person2.GetFullName("Mr.")}");
+
+//6.
+Person myself = new Person();
+myself.firstName = "Jesper";
+myself.lastName = "Stranne";
+
+Person mother = new Person();
+mother.firstName = "Lena";
+mother.lastName = "Johansson";
+
+myself.mother = mother;
+
+Person father = new Person();
+father.firstName = "Mikael";
+father.lastName = "Alveberg";
+
+myself.father = father;
+
+Console.WriteLine($"Mitt namn är: {myself.GetFullName()}");
+Console.WriteLine($"Min mammas namn är: {myself.mother.GetFullName()}");
+Console.WriteLine($"Min pappas namn är: {myself.father.GetFullName()}");
+Console.WriteLine("**********");
+Console.WriteLine(myself.GetSelfAndParents());
+
 
 class Person
 {
     public string firstName = "Default name";
     public string lastName = "Default name";
     public string fullName = "Default name";
+
+    // Varje Person kan ha två referenser till Person-Objekt (father, mother)
+    public Person father;
+    public Person mother;
 
     public string GetFullName() // Returnerar hela namnet
     {
@@ -43,6 +66,22 @@ class Person
         return this.fullName;
     }
 
+    //7.
+    // Lägg till en public string GetSelfAndParents() som returnerar en sträng på formatet
+    // "Ditt Namn - Mamma: Mammans Namn - Pappa: Pappans namn". Om mammans eller pappans namn
+    // saknas (null) markera det med "okänd" i strängen. Exempel: "Fredrik Johansson - Mamma: okänd - Pappa: Göran Johansson"
+    public string GetSelfAndParents()
+    {
+        // ??
+        // If mamma är null = skriv ut mammans namn som null
+        //
+
+        return $"{this.GetFullName()} - Mamma: {this.mother.GetFullName()} - Pappa: {this.father.GetFullName()}";
+
+    }
+
+   
+
 }
 
 
@@ -53,7 +92,6 @@ class Person
 
 // 2. Lägg till en metod som returnerar hela namnet
 // Uppdatera klassen i uppgift 1 med en metod: public string GetFullName(), som returnerar en sträng med hela namnet.
-
 
 // 3. Lägg till en metod som returnerar namnet baklänges
 //Uppdatera klassen med en ny metod: public string GetFullNameReversed()
@@ -69,3 +107,12 @@ class Person
 //Dessa representeras som publika fält(fields) av typen Person
 //(eller vad du kallade klassen i uppgift 1).
 
+// 6. Skapa ett objekt som representerar dig och dina föräldrar
+// Gör en instans av en person och ge den ditt för- och efternamn.
+// Lägg även in dina föräldrars namn, kopplade till dig. Så t.ex myself.GetFullName()
+// returnerar ditt namn, och myself.mother.GetFullName() returnerar din mammas namn.
+
+// 7. 
+// Lägg till en public string GetSelfAndParents() som returnerar en sträng på formatet
+// "Ditt Namn - Mamma: Mammans Namn - Pappa: Pappans namn". Om mammans eller pappans namn
+// saknas (null) markera det med "okänd" i strängen. Exempel: "Fredrik Johansson - Mamma: okänd - Pappa: Göran Johansson"
