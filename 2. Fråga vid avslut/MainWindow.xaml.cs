b8bca638a2c582be425f61d3044285cacb9e9348
Jesper johansson <jesalv0511@hotmail.com>
2024-10-20 22:56:25 +0200

    Dialogrutor 1-3

---
+++ 2. Fråga vid avslut/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2._Fråga_vid_avslut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to quit?", "Main window", MessageBoxButton.YesNo);
+            
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Application started", "Main application");
+        }
+    }
+}
---
+++ 3. App för att öppna och visa textfiler/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3._App_för_att_öppna_och_visa_textfiler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string ?TextBox { get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void menuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+
+            var result = dialog.ShowDialog();
+
+            if (result == true )
+            {
+                myTextBox.Text = System.IO.Path.GetFileName(dialog.FileName);
+            }
+        }
+
+        private void menuExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "File application", MessageBoxButton.YesNo);
+            
+            if (result == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+
+            return;
+
+        }
+    }
+}
